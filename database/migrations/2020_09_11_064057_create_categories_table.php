c8abeb7474b86118618aeb3b544fa3b81d86fca5
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2020-09-11 15:45:40 +0700

    API-Wartegku

---
+++ database/migrations/2020_09_11_064057_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('store_id')->unsigned();
+            $table->string('category');
+            $table->boolean('is_food');
+            $table->timestamps();
+
+            $table->foreign('store_id')->references('id')->on('stores')->onDelete('CASCADE');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
