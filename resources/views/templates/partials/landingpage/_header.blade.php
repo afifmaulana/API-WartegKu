e761b7c5e74ae0ef1c2e96ac7fae055a2948e7f6
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2020-09-19 07:13:47 +0700

    Landingpage

--- resources/views/templates/partials/landingpage/_header.blade.php
+++ resources/views/templates/partials/landingpage/_header.blade.php
@@ -6,6 +6,7 @@
             <div class="col-sm-12">
                 <div class="main-menu">
                     <div class="menu-left">
+
                         <div class="brand-logo">
                             <a href="index.html"><img src="{{ asset ('assets-landingpage/images/icon/logo.png') }}"
                                                       class="img-fluid blur-up lazyload" alt=""></a>
@@ -16,7 +17,11 @@
                             <nav id="main-nav">
                                 <div class="toggle-nav"><i class="fa fa-bars sidebar-bar"></i></div>
                                 <ul id="main-menu" class="sm pixelstrap sm-horizontal">
-                                    <li><a href="#">Home</a></li>
+                                    <li>
+                                        <div class="mobile-back text-right">Back<i class="fa fa-angle-right pl-2"
+                                                aria-hidden="true"></i></div>
+                                    </li>
+                                    <li><a href="">Home</a></li>
                                     <li><a href="#">shop</a></li>
                                     <li><a href="#">product</a></li>
                                     <li><a href="#">pages</a></li>
