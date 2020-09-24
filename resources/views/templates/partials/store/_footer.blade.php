a1694f2674926c8caa9bd786e71912ad11141c36
Afif Maulana Iskandar <afifmaulanaiskandar@gmail.com>
2020-09-24 16:21:31 +0700

    Landing Page

--- resources/views/templates/partials/store/_footer.blade.php
+++ resources/views/templates/partials/store/_footer.blade.php
@@ -4,9 +4,7 @@
             <div class="col-md-6 footer-copyright">
                 <p class="mb-0">2020 WartegKu | Ayo Jalin Hubungan Denganku</p>
             </div>
-            <div class="col-md-6">
-                <p class="pull-right mb-0">Hand crafted & made with<i class="fa fa-heart"></i></p>
-            </div>
+          
         </div>
     </div>
 </footer>
